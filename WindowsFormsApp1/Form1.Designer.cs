e0d999f61b1b74cbb95799cc1c9f9aa7930e65a4
Mario Month <Mario.month@gmail.com>
2017-11-15 19:37:51 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Bits:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(474, 39);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(762, 368);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(754, 342);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Amplitude Shift Keying";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(754, 342);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Frequency Shift Keying";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Digital Signal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Analog Signal";
+            // 
+            // chart3
+            // 
+            chartArea3.AxisX.Interval = 1D;
+            chartArea3.AxisX.LineWidth = 0;
+            chartArea3.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea3.AxisX.MajorTickMark.Enabled = false;
+            chartArea3.AxisX.Minimum = 1D;
+            chartArea3.AxisY.Interval = 1D;
+            chartArea3.AxisY.LabelStyle.Enabled = false;
+            chartArea3.AxisY.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            chartArea3.AxisY.MajorGrid.Enabled = false;
+            chartArea3.AxisY.MajorTickMark.Enabled = false;
+            chartArea3.AxisY.Maximum = 1D;
+            chartArea3.AxisY.Minimum = -1D;
+            chartArea3.BorderColor = System.Drawing.Color.Transparent;
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(12, 139);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(456, 268);
+            this.chart3.TabIndex = 7;
+            this.chart3.Text = "chart3";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisX.Minimum = 1D;
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.LabelStyle.Enabled = false;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.Maximum = 1D;
+            chartArea1.AxisY.Minimum = -1D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(4, 7);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(744, 329);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.AxisX.Interval = 1D;
+            chartArea2.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea2.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea2.AxisX.MajorTickMark.Enabled = false;
+            chartArea2.AxisX.Minimum = 1D;
+            chartArea2.AxisY.LabelStyle.Enabled = false;
+            chartArea2.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea2.AxisY.MajorGrid.Enabled = false;
+            chartArea2.AxisY.MajorTickMark.Enabled = false;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(7, 4);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(741, 335);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 419);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Mario Month - CSC567 - Assignment 1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+
+{
+
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        bool[] bits;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart3.Series[0].Points.Clear();
+
+            // Check for valid input
+            bool valid = validateInput(textBox1.Text);
+
+            if (valid)
+            {
+                // Convert string to boolean values
+                bits = textBox1.Text.Select(c => c == '1').ToArray();
+
+                // Process Differential Manchester Encoding
+                processDME(bits);
+
+                // Process Amplitude Shift Keying
+                processASK(bits);
+
+                // Process Frequency Shift Keying
+                processFSK(bits);
+            }
+
+            textBox1.Clear();
+        }
+
+        private bool validateInput(string s)
+        {
+            // Error checking: must ensure text is not empty
+            if (s.Length == 0)
+            {
+                MessageBox.Show("Please enter bits!");
+                return false;
+            }
+
+            // Error checking: must ensure entered text are valid bits
+            bool isValidBits = true;
+            foreach (char c in s)
+            {
+                if (c != '0' && c != '1')
+                    isValidBits = false;
+            }
+
+            if (isValidBits == false)
+            { 
+                MessageBox.Show("Error: Must only enter a combination of 0's and 1's!");
+                return false;
+            }
+            return true;
+        }
+
+        private void processDME(bool[] booleans)
+        {
+            // Local Variables
+            List<float> xData = new List<float>();
+            List<float> yData = new List<float>();
+            float firstPointX = 0;
+            float secondPointX = 0;
+            float thirdPointX = 0;
+            float fourthPointX = 0;
+            float fifthPointX = 0;
+            float firstPointY = 0;
+            float secondPointY = 0;
+            float thirdPointY = 0;
+            float fourthPointY = 0;
+            float fifthPointY = 0;
+
+            // If the first bit is a 0
+            if (booleans[0] == false)
+            {
+                firstPointX = 1;
+                firstPointY = 1;
+                secondPointX = firstPointX;
+                secondPointY = -firstPointY;
+                thirdPointX = secondPointX + 0.5f;
+                thirdPointY = secondPointY;
+                fourthPointX = thirdPointX;
+                fourthPointY = -thirdPointY;
+                fifthPointX = thirdPointX + 0.5f;
+                fifthPointY = fourthPointY;
+
+                xData.Add(firstPointX);
+                xData.Add(secondPointX);
+                xData.Add(thirdPointX);
+                xData.Add(fourthPointX);
+                xData.Add(fifthPointX);
+
+                yData.Add(firstPointY);
+                yData.Add(secondPointY);
+                yData.Add(thirdPointY);
+                yData.Add(fourthPointY);
+                yData.Add(fifthPointY);
+            }
+            else // Otherwise, first bit is a 1
+            {
+                firstPointX = 1;
+                firstPointY = 1;
+                secondPointX = firstPointX + 0.5f;
+                secondPointY = firstPointY;
+                thirdPointX = secondPointX;
+                thirdPointY = -secondPointY;
+                fourthPointX = thirdPointX + 0.5f;
+                fourthPointY = thirdPointY;
+
+                xData.Add(firstPointX);
+                xData.Add(secondPointX);
+                xData.Add(thirdPointX);
+                xData.Add(fourthPointX);
+
+                yData.Add(firstPointY);
+                yData.Add(secondPointY);
+                yData.Add(thirdPointY);
+                yData.Add(fourthPointY);
+            }
+            
+            for (int i=1; i<booleans.Length; i++)
+            {
+                // If the current bit is a 1
+                if (booleans[i] == true)
+                {
+                    if (booleans[i - 1] == false)
+                    {
+                        firstPointX = fifthPointX;
+                        firstPointY = fifthPointY;
+                    }
+                    else
+                    {
+                        firstPointX = fourthPointX;
+                        firstPointY = fourthPointY;
+                    }
+
+                    secondPointY = firstPointY;
+                    secondPointX = firstPointX + 0.5f;
+                    thirdPointX = secondPointX;
+                    thirdPointY = -secondPointY;
+                    fourthPointX = thirdPointX + 0.5f;
+                    fourthPointY = thirdPointY;
+
+                    xData.Add(firstPointX);
+                    xData.Add(secondPointX);
+                    xData.Add(thirdPointX);
+                    xData.Add(fourthPointX);
+
+                    yData.Add(firstPointY);
+                    yData.Add(secondPointY);
+                    yData.Add(thirdPointY);
+                    yData.Add(fourthPointY);
+                }
+                else // Otherwise, the current bit is a 0
+                {
+                    if (booleans[i - 1] == false)
+                    {
+                        firstPointX = fifthPointX;
+                        firstPointY = fifthPointY;
+                    }
+                    else
+                    {
+                        firstPointX = fourthPointX;
+                        firstPointY = fourthPointY;
+                    }
+
+                    secondPointY = -firstPointY;
+                    secondPointX = firstPointX;
+                    thirdPointX = secondPointX + 0.5f;
+                    thirdPointY = secondPointY;
+                    fourthPointX = thirdPointX;
+                    fourthPointY = -thirdPointY;
+                    fifthPointX = fourthPointX + 0.5f;
+                    fifthPointY = fourthPointY;
+
+                    xData.Add(firstPointX);
+                    xData.Add(secondPointX);
+                    xData.Add(thirdPointX);
+                    xData.Add(fourthPointX);
+                    xData.Add(fifthPointX);
+
+                    yData.Add(firstPointY);
+                    yData.Add(secondPointY);
+                    yData.Add(thirdPointY);
+                    yData.Add(fourthPointY);
+                    yData.Add(fifthPointY);
+                }
+            }
+
+            // Add data to DME chart
+            chart3.Series[0].Points.DataBindXY(xData, yData);
+        }
+
+        private void processASK(bool[] booleans)
+        {
+            for (int i=0; i < booleans.Length; i++)
+            {
+                if (booleans[i] == false)
+                {
+                    chart1.Series[0].Points.AddXY(i + 1, 0);
+                    chart1.Series[0].Points.AddXY(i + 2, 0);
+                }
+                else
+                {
+                    for (double x = i+1; x < i+2; x += 0.001)
+                    {
+                        double yValue = Math.Sin(4 * x * 2 * Math.PI);
+                        chart1.Series[0].Points.AddXY(x, yValue);
+                    }
+                }
+            }
+        }
+
+        private void processFSK(bool[] booleans)
+        {
+            for (int i = 0; i < booleans.Length; i++)
+            {
+                if (booleans[i] == false)
+                {
+                    for (double x = i + 1; x < i + 2; x += 0.001)
+                    {
+                        double yValue = Math.Sin(2 * x * 2 * Math.PI);
+                        chart2.Series[0].Points.AddXY(x, yValue);
+                    }
+                }
+                else
+                {
+                    for (double x = i + 1; x < i + 2; x += 0.001)
+                    {
+                        double yValue = Math.Sin(4 * x * 2 * Math.PI);
+                        chart2.Series[0].Points.AddXY(x, yValue);
+                    }
+                }
+            }
+        }
+    }
+}
